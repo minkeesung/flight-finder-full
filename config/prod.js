568c55880e462ec5dee0dd4917c7b9bbcbeda4e2
Minkee Sung <minkeesung@gmail.com>
2018-01-11 23:37:45 -0500

    sodfisdj

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,5 @@
 // prod.js - this productino keys are here!!!
 
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: 'mongodb://minkeesung:password@ds251197.mlab.com:51197/flight_finder_dev',
-  cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN
+  mongoURI: 'mongodb://minkeesung:password@ds251197.mlab.com:51197/flight_finder_dev'
 };
