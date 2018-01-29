c5b2e64407be4f94e2de3df1bbfc196a3dae933f
Minkee Sung <minkeesung@gmail.com>
2018-01-28 22:13:06 -0500

    changed to app.listen

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000
-const server = http.createServer(app)
 
-server.listen(PORT)
+app.listen(PORT)
 console.log('server listening on:', PORT)
